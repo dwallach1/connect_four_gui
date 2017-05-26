687dc441310dac1cb7424ab8ebfd339729c0faea
kevin <kevinqjin@gmail.com>
2017-05-26 00:43:15 -0500

    dynamic sizing of board works

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,27 +1,29 @@
-use gtk::{self};
-use gtk::traits::*;
-use gtk::prelude::*;
+// use gtk::{self};
+// use gtk::traits::*;
+// use gtk::prelude::*;
+use gtk::*;
+
 
 //  GTK+ is not thread-safe. Accordingly, none of this crate's structs implement Send or Sync.
 
-fn configure_window(window: &gtk::Window) {
+fn configure_window(window: &Window) {
 	window.set_title("Connect Four Game Server");
 	let(width, height) = (500, 400);
 	window.set_default_size(width,height);  
 	window.connect_delete_event(|_,_| {
-    	gtk::main_quit();
-    	gtk::Inhibit(false)
+    	main_quit();
+    	Inhibit(false)
     	
 	});
 } 
 
-fn configure_game_window(window: &gtk::Window) {
+fn configure_game_window(window: &Window) {
 	window.set_title("Connect Four Game Server");
 	let(width, height) = (750, 650);
 	window.set_default_size(width,height);  
 	window.connect_delete_event(|_,_| {
-    	gtk::main_quit();
-    	gtk::Inhibit(false)
+    	main_quit();
+    	Inhibit(false)
     	
 	});
 } 
@@ -32,75 +34,80 @@ fn configure_game_window(window: &gtk::Window) {
 
 fn end_turn() {
 	let game_glade_src = include_str!("game_window.glade");
-	let game_builder = gtk::Builder::new_from_string(game_glade_src);
-	let play_btn: gtk::Button = game_builder.get_object("play_btn").unwrap();
+	let game_builder = Builder::new_from_string(game_glade_src);
+	let play_btn: Button = game_builder.get_object("play_btn").unwrap();
 	play_btn.hide();
 }
 
 fn build_game_window() {
-	let game_glade_src = include_str!("game_window.glade");
-	let game_builder = gtk::Builder::new_from_string(game_glade_src);
-	let game_window: gtk::Window = game_builder.get_object("window1").unwrap();
+	let game_glade_src = include_str!("test.glade");
+	let game_builder = Builder::new_from_string(game_glade_src);
+	let game_window: Window = game_builder.get_object("game_window").unwrap();
 	configure_game_window(&game_window);
 
-	// add radio buttons
-	let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 0);
-    let hbox_inner = gtk::Box::new(gtk::Orientation::Horizontal, 0);
-    hbox_inner.set_spacing(34);
+	let k = 6;
+	let game_board = Grid::new();
+	game_board.set_column_spacing(35);
+	game_board.set_row_spacing(35);
 
-    let base = gtk::RadioButton::new_with_label_from_widget(None, "-1");
-    let col_1 = gtk::RadioButton::new_with_label_from_widget(Some(&base), "1");
-    let col_2 = gtk::RadioButton::new_with_label_from_widget(Some(&base), "2");
-    let col_3 = gtk::RadioButton::new_with_label_from_widget(Some(&base), "3");
-    let col_4 = gtk::RadioButton::new_with_label_from_widget(Some(&base), "4");
-    let col_5 = gtk::RadioButton::new_with_label_from_widget(Some(&base), "5");
-    let col_6 = gtk::RadioButton::new_with_label_from_widget(Some(&base), "6");
-    let col_7 = gtk::RadioButton::new_with_label_from_widget(Some(&base), "7");
+	for i in 0..k {
+		for j in 0..k {
+			let image = Image::new_from_file("empty.png");
+			game_board.attach(&image, i, j, 1, 1);
+		}
+	}
+
+	let play_button = Button::new_with_label("Play");
+
+	// add radio buttons
+    let radio_box = Box::new(Orientation::Horizontal, 0);
+    let base = RadioButton::new_with_label_from_widget(None, "-1");
+    for i in 1..k+1 {
+    	let btn = RadioButton::new_with_label_from_widget(Some(&base), &i.to_string());
+    	radio_box.pack_start(&btn, false, false, 25);
+    }
+
+	let game_box: Box = game_builder.get_object("game_box").unwrap();
+	game_box.pack_start(&game_board, true, true, 20);
+	game_box.pack_start(&radio_box, true, true, 20);
+	game_box.pack_start(&play_button, false, true, 20);
  
-    hbox_inner.pack_start(&col_1, false, false, 0);
-    hbox_inner.pack_start(&col_2, false, false, 0);
-    hbox_inner.pack_start(&col_3, false, false, 0);
-    hbox_inner.pack_start(&col_4, false, false, 0);
-    hbox_inner.pack_start(&col_5, false, false, 0);
-    hbox_inner.pack_start(&col_6, false, false, 0);
-    hbox_inner.pack_start(&col_7, false, false, 0);
- 
-    let radio_button_group = vec![col_1, col_2, col_3, col_4, col_5, col_6, col_7];
+ //    let radio_button_group = vec![col_1, col_2, col_3, col_4, col_5, col_6, col_7];
     
-    hbox.pack_end(&hbox_inner, false, false, 0);
-    let container: gtk::Fixed = game_builder.get_object("fixed1").unwrap();
-    container.add(&hbox);
-    hbox.set_margin_top(500);
-    hbox.set_margin_bottom(50);
-    hbox.set_margin_start(50);
-    hbox.set_margin_end(150);
+ //    hbox.pack_end(&hbox_inner, false, false, 0);
+ //    let container: Fixed = game_builder.get_object("fixed1").unwrap();
+ //    container.add(&hbox);
+ //    hbox.set_margin_top(500);
+ //    hbox.set_margin_bottom(50);
+ //    hbox.set_margin_start(50);
+ //    hbox.set_margin_end(150);
 
 	game_window.show_all();
 
-	let play_btn: gtk::Button = game_builder.get_object("play_btn").unwrap();
-	play_btn.connect_clicked(move |_| {
-		for button in &radio_button_group {
-			if button.get_active() {
-				// play the move here
-				// play_move(int(button.get_label().unwrap());
-				// add functionality when we connect oData library and game functionality
-				println!("{:?}", button.get_label().unwrap());
-				end_turn();
-				break;
-				// let img_name = "piece_" + str(x) + str(y) ;
-				// ler curr_img = game_builder.get_object(img_name).unwrap();
-				// if player1 { curr_img.set_image("red_piece.png") }
-				// else { curr_img.set_image("blue_piece.png") }	
-			}
-		}		
-		println!("{:?}", String::from("passed out of toggle loop"));
-	});
+	// let play_btn: Button = game_builder.get_object("play_btn").unwrap();
+	// play_btn.connect_clicked(move |_| {
+	// 	for button in &radio_button_group {
+	// 		if button.get_active() {
+	// 			// play the move here
+	// 			// play_move(int(button.get_label().unwrap());
+	// 			// add functionality when we connect oData library and game functionality
+	// 			println!("{:?}", button.get_label().unwrap());
+	// 			end_turn();
+	// 			break;
+	// 			// let img_name = "piece_" + str(x) + str(y) ;
+	// 			// ler curr_img = game_builder.get_object(img_name).unwrap();
+	// 			// if player1 { curr_img.set_image("red_piece.png") }
+	// 			// else { curr_img.set_image("blue_piece.png") }	
+	// 		}
+	// 	}		
+	// 	println!("{:?}", String::from("passed out of toggle loop"));
+	// });
 
 
-	let quit_btn: gtk::Button = game_builder.get_object("button3").unwrap();
+	let quit_btn: Button = game_builder.get_object("quit_button").unwrap();
 	quit_btn.connect_clicked(move |_| {
-		gtk::main_quit();
-    	gtk::Inhibit(false);
+		main_quit();
+    	Inhibit(false);
 	});
 
 }
@@ -109,16 +116,16 @@ fn build_game_window() {
 pub fn launch() {     
 
 	// first step: initalize GTK
-	gtk::init().unwrap_or_else(|_| panic!("Panic, unable to initalize GTK!"));	
+	init().unwrap_or_else(|_| panic!("Panic, unable to initalize GTK!"));	
 
 	// initalize main window
 	let glade_src = include_str!("app_window.glade");
-	let builder = gtk::Builder::new_from_string(glade_src);
-	let app_window: gtk::Window = builder.get_object("window1").unwrap();
+	let builder = Builder::new_from_string(glade_src);
+	let app_window: Window = builder.get_object("window1").unwrap();
 	configure_window(&app_window);
 
 	// add closure to connect button to open new (game) screen
-	let connect_btn: gtk::Button = builder.get_object("button1").unwrap();
+	let connect_btn: Button = builder.get_object("button1").unwrap();
 	connect_btn.connect_clicked(move |_| {
 		// build and bring game window to view
 		build_game_window();
@@ -126,14 +133,14 @@ pub fn launch() {
 	});
 
 	// add closure to quit application when this button is pressed
-	let quit_btn: gtk::Button = builder.get_object("button2").unwrap();
+	let quit_btn: Button = builder.get_object("button2").unwrap();
 	quit_btn.connect_clicked(move |_| {
-		gtk::main_quit();
-    	gtk::Inhibit(false);
+		main_quit();
+    	Inhibit(false);
 	});
 
 	// bring the window to view and start the application
 	app_window.show_all(); 
-	gtk::main();	
+	main();	
 }
 
